4385e2f14fbe80f709250d85249455c08ec29b50
feno <fenosoarafidinirina@gmail.com>
2026-06-24 10:57:02 +0200

    modif lien css

--- Projet-Laravel/resources/views/components/auth-layout.blade.php
+++ Projet-Laravel/resources/views/components/auth-layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('assets/styles/main.scss') }}">
+    <link rel="stylesheet" href="{{ asset('assets/styles/main.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
