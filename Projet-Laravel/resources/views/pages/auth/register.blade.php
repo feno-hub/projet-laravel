310485efeb27e53fa887281aceac18586ea170a8
feno <fenosoarafidinirina@gmail.com>
2026-06-27 15:49:39 +0200

    modif register

--- Projet-Laravel/resources/views/pages/auth/register.blade.php
+++ Projet-Laravel/resources/views/pages/auth/register.blade.php
@@ -1,4 +1,6 @@
-<div>
-    <h1>PAGES INSCRIPTIONS</h1>
-    <a href="{{ route('loginForm') }}">connexion</a>
-</div>
+<x-auth-layout>
+    <div>
+        <h1>PAGES INSCRIPTIONS</h1>
+        <a href="{{ route('loginForm') }}">connexion</a>
+    </div>
+</x-auth-layout>
