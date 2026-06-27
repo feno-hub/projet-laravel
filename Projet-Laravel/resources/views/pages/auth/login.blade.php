660a1727ef2f8f92616538fbeeda5d01b0e47df4
feno <fenosoarafidinirina@gmail.com>
2026-06-27 17:18:00 +0200

    modif css

--- Projet-Laravel/resources/views/pages/auth/login.blade.php
+++ Projet-Laravel/resources/views/pages/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-auth-layout>
     <div class="">
-        <h1 class="login">PAGES CONNEXIONS</h1>
+        <h1 class="auth-login">PAGES CONNEXIONS</h1>
         <a href="{{ route('registerForm') }}">inscription</a>
     </div>
 </x-auth-layout>
