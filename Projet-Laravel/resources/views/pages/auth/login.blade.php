62e944242d92203d097660cafb1ac81d321f0937
feno <fenosoarafidinirina@gmail.com>
2026-06-27 17:42:06 +0200

    modif css

--- Projet-Laravel/resources/views/pages/auth/login.blade.php
+++ Projet-Laravel/resources/views/pages/auth/login.blade.php
@@ -1,6 +1,28 @@
 <x-auth-layout>
-    <div class="">
-        <h1 class="auth-login">PAGES CONNEXIONS</h1>
-        <a href="{{ route('registerForm') }}">inscription</a>
-    </div>
+
+    <h1 class="auth-title">se connecter</h1>
+
+    <form action="" method="post" class="auth-login">
+
+        <div class="auth-login-lien">
+            <h2 class="auth-login-lien-title1">connexion</h2>
+            <h2 class="auth-login-lien-title2">inscription</h2>
+        </div>
+
+        <div class="auth-login-parent">
+            <label for="" class="auth-login-parent-label">email</label>
+            <input type="email" name="email" id="" placeholder="Email ..."
+                class="auth-login-parent-input">
+        </div>
+
+        <div class="auth-login-parent">
+            <label for="" class="auth-login-parent-label">email</label>
+            <input type="password" name="password" id="" placeholder="Password ..."
+                class="auth-login-parent-input">
+        </div>
+
+        <button type="submit" class="auth-login-btn">se connecter</button>
+
+    </form>
+
 </x-auth-layout>
